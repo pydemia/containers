8b55d63d30243ee0c52279ea58b6c28508e847f7
Youngju Jaden Kim <pydemia@gmail.com>
2024-03-20 15:13:56 +0900

    Update cri-purge.sh

--- kubernetes/commands/cri-purge.sh
+++ kubernetes/commands/cri-purge.sh
@@ -9,4 +9,4 @@ sudo cri-purge --purge
 # just in case of `Error while dialing dial unix /var/run/dockershim.sock: connect: no such file or directory"`:
 # WARN[0000] image connect using default endpoints: [unix:///var/run/dockershim.sock unix:///run/containerd/containerd.sock unix:///run/crio/crio.sock unix:///var/run/cri-dockerd.sock]. As the default settings are now deprecated, you should set the endpoint instead.
 # ERRO[0000] validate service connection: validate CRI v1 image API for endpoint "unix:///var/run/dockershim.sock": rpc error: code = Unavailable desc = connection error: desc = "transport: Error while dialing dial unix /var/run/dockershim.sock: connect: no such file or directory"
-crictl config --set runtime-endpoint=unix:///run/containerd/containerd.sock --set image-endpoint=unix:///run/containerd/containerd.sock
+sudo crictl config --set runtime-endpoint=unix:///run/containerd/containerd.sock --set image-endpoint=unix:///run/containerd/containerd.sock
